f25e67843d152be464981a3a6bd627d014cf1e43
edubew <edubew98@gnail.com>
2024-03-25 14:49:12 +0300

    Add a transactions migration

---
+++ db/migrate/20240325114435_create_transactions.rb
@@ -0,0 +1,13 @@
+class CreateTransactions < ActiveRecord::Migration[7.1]
+  def change
+    create_table :transactions do |t|
+      t.references :user, foreign_key: true
+      t.references :category, foreign_key: true
+      t.decimal :amount
+      t.date :date
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
