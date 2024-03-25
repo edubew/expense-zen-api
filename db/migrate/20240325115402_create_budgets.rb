e8f23fb798095c2270b3336b8f2e1f59814499b1
edubew <edubew98@gnail.com>
2024-03-25 14:57:08 +0300

    Add a budgets migration

---
+++ db/migrate/20240325115402_create_budgets.rb
@@ -0,0 +1,14 @@
+class CreateBudgets < ActiveRecord::Migration[7.1]
+  def change
+    create_table :budgets do |t|
+      t.references :users, foreign_key: true
+      t.references :category, foreign_key: true
+      t.decimal :amount
+      t.date: start_date
+      t.date: end_date
+      t.string :period
+
+      t.timestamps
+    end
+  end
+end
