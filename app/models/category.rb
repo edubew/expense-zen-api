97ee16d4998b30079d505d4577dda767ba851e48
edubew <edubew98@gnail.com>
2024-03-28 18:13:34 +0300

    Add validations for the category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,12 @@
 class Category < ApplicationRecord
   # Associations
-  has_many :transactions
+  has_many :transactions, dependent: :destroy
+
+  # Validations
+  validates :name, presence: true
+  validates :icon, presence: true
+
+  def total_amount
+    transactions.sum(:amount)
+  end
 end
