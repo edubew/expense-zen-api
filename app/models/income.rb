a675f643b52671e3fba8d824b7cba03b4d65055d
edubew <edubew98@gnail.com>
2024-03-28 18:18:05 +0300

    Add validations for the income model

--- app/models/income.rb
+++ app/models/income.rb
@@ -1,4 +1,9 @@
 class Income < ApplicationRecord
   # Associations
   belongs_to :user
+
+  # Validations
+  validates :amount, presence: true, numericality: { greater_than: 0}
+  validates :source, presence: true
+  validates :date, presence: true
 end
