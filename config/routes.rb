10d600eef6e303242cf5a951a5c5863ce9575d80
edubew <edubew98@gnail.com>
2024-03-28 17:58:42 +0300

    Configure routes endpoints to manage categories, transactions, incomes and goals in the API

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,17 @@ Rails.application.routes.draw do
   
   get '/current_user' => 'current_user#index'
 
+  namespace :api do
+    namespace :v1 do
+      resources :categories, only: [:create, :index, :update, :destroy]
+      resources :transactions, only: [:show, :update, :destroy]
+      resources :incomes, only: [:create, :index, :update, :destroy]
+      resources :goals, only: [:create, :index, :show, :update, :destroy]
+
+      resources :categories do
+        resources :transactions, only: [:index, :create]
+      end
+    end
+  end
   # get "up" => "rails/health#show", as: :rails_health_check
 end
