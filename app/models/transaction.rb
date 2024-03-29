4f1aa4cff7803a351145fbfe5de139ee7553347a
edubew <edubew98@gnail.com>
2024-03-29 14:31:42 +0300

    Add validations for the transactions model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,4 +2,9 @@ class Transaction < ApplicationRecord
   # Associations
   belongs_to :user
   belongs_to :category
+
+  # Validations
+  validates :date, presence: true
+  validates :description, presence: true
+  validates :amount, presence: true, numericality: { greater_than: 0 }
 end
