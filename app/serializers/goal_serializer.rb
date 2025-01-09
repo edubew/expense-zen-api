87e7a2be1a9255b3584567c94766a792d0ca600b
edubew <edubew98@gmail.com>
2025-01-09 13:57:43 +0300

    Add goal_serializer to explitly set the types for the required params

---
+++ app/serializers/goal_serializer.rb
@@ -0,0 +1,11 @@
+class GoalSerializer < ActiveModel::Serializer
+  attributes :id, :user_id, :amount, :start_date, :end_date, :created_at, :updated_at, :name, :status, :progress, :progress_difference, :status_message
+
+  def amount
+    object.amount.to_f
+  end
+
+  def progress
+    object.progress.to_f
+  end
+end
